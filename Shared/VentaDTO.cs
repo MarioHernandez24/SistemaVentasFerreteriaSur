1d9d7f72ce44f9db3559aa924afeee733f480e33
Mario Hernandez <geovanyhernandezarias@gmail.com>
2024-11-05 19:49:08 -0600

    Agregados los principales DTOs a usar

---
+++ Shared/VentaDTO.cs
@@ -0,0 +1,31 @@
+﻿using FerreteriaSur.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerreteriaSur.Shared
+{
+    public class VentaDTO
+    {
+        public int IdVenta { get; set; }
+        public string? NumeroDocumento { get; set; }
+        public string? TipoPago { get; set; }
+        public DateTime? FechaRegistro { get; set; }
+        public decimal? Total { get; set; }
+        public string? TotalTexto
+        {
+
+            get
+            {
+                decimal? sum = 0;
+                if (DetalleVenta.Count > 0)
+                    sum = DetalleVenta.Sum(p => p.Total);
+
+                return sum.ToString();
+            }
+        }
+        public virtual List<DetalleVentaDTO>? DetalleVenta { get; set; }
+    }
+}
