add44d03ca277d466ae0ad9666a4ee926b046eaf
Mario Hernandez <geovanyhernandezarias@gmail.com>
2024-11-05 20:34:43 -0600

    Agregados archivos de utilidades

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,27 @@
+global using FerreteriaSur.Client.Servicios.Contrato;
+global using FerreteriaSur.Shared;
+using CurrieTechnologies.Razor.SweetAlert2;
 using FerreteriaSur.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using FerreteriaSur.Client;
+using FerreteriaSur.Client.Servicios.Implementacion;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<ICategoriaService, CategoriaService>();
+builder.Services.AddScoped<IProductoService, ProductoService>();
+builder.Services.AddScoped<IRolService, RolService>();
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<IVentaService, VentaService>();
+builder.Services.AddScoped<IDashBoardService, DashBoardService>();
+
+builder.Services.AddMudServices();
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
